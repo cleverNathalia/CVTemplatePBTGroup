9cece1936069b175d0a96cc9964d76ebded7b9c8
cleverNathalia <danielledramaqueeeen@gmail.com>
2019-01-15 16:38:24 +0200

    Made changes

---
+++ js/education/trainingHeader.js
@@ -0,0 +1,27 @@
+$("#trainingHeader").append(
+
+    '<div class="col-lg-4 col-md-4 col-sm-12 alignLeft">' +
+    '<div class="sectionHeader">' +
+    '<b>' +
+    'TRAINING' +
+    '</b>' +
+    '<br>' +
+    '</div>' +
+    '</div>' +
+
+    '<div class="col-lg-4 col-md-4 col-sm-12 alignCenter">' +
+    ' <div class="sectionHeader">' +
+    '<b>' +
+    'INSTITUTION' +
+    '</b>' +
+    '</div>' +
+    '</div>' +
+
+    '<div class="col-lg-4 col-md-4 col-sm-12 alignRight">' +
+    '<div class="sectionHeader">' +
+    '<b>' +
+    'YEAR' +
+    '</b>  ' +
+    '</div>' +
+    '</div>'
+);
